ce77b267390cf493bbfcc921b51ea6cbe4c64a19
Paloma~ <palomasol201@gmail.com>
2023-04-13 20:17:38 -0400

    Add planets animations

--- Stardust-Rescue-Run/Planeta2.gd
+++ Stardust-Rescue-Run/Planeta2.gd
@@ -4,7 +4,7 @@ extends Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	animation_player.play("tierra")
+	animation_player.play("marte")
 	pass # Replace with function body.
 
 
