aedc396f58b0dae79cc30825a07371aadfdbf8ec
Angélica Hernández D <angelica.lo333@gmail.com>
2023-04-24 10:30:18 -0400

    Blast scene created
    Aun falta añadirla a la escena principal para que vaya destruyen2 el mapa

---
+++ Stardust-Rescue-Run/Blast.gd
@@ -0,0 +1,26 @@
+extends Area2D
+
+@onready var area = $"."
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	$CollisionShape2D.shape.radius += 0.2
+
+func _input(event):
+	#for body in area.get_overlapping_bodies():
+	#	body.queue_free()
+	pass
+
+func destroy(body):
+	
+	pass # Replace with function body.
+
+
+func _on_body_entered(body):
+	body.queue_free()
+	pass # Replace with function body.
