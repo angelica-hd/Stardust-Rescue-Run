794781b8348b6858ad3daeb05987e988e414bca1
Paloma~ <palomasol201@gmail.com>
2023-05-17 16:09:36 -0400

    Add new alien, planet sprites and scenes

--- Stardust-Rescue-Run/script/monito.gd
+++ Stardust-Rescue-Run/script/monito.gd
@@ -1,12 +1,15 @@
 class_name Monito
 extends RigidBody2D
 
-@export_enum("Juan", "Juana") var tipo_monito : int
-@export var texturas : Array
+##@export_enum("Juan", "Juana") var tipo_monito : int
+##@export var texturas : Array
+@onready var animation_player = $AnimationPlayer
+@onready var icon = $Icon
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Icon.texture = texturas[tipo_monito]
+	#$Icon.texture = texturas[tipo_monito]
+	animation_player.play("monito1")
 	pass # Replace with function body.
 
 
