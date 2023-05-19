1af105e77d4a191452a17dcd6cd6ec0341987665
Paloma~ <palomasol201@gmail.com>
2023-05-19 08:51:06 -0400

    Add more "monitos" and portal animation

--- Stardust-Rescue-Run/script/portal.gd
+++ Stardust-Rescue-Run/script/portal.gd
@@ -1,4 +1,10 @@
 extends Area2D
+@onready var portal_animate = $portal_animate
+
+func _ready():
+	#$Icon.texture = texturas[tipo_monito]
+	portal_animate.play("portal1")
+	pass # Replace with function body.
 
 func _on_portal_body_entered(body):
 	if body.name == "SPACESHIP":
