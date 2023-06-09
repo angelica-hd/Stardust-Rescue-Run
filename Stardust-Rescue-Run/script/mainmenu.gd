90f167ef9b41695be0c324030ffec73162283783
Angélica Hernández D <angelica.lo333@gmail.com>
2023-06-09 18:16:54 -0400

    Credits function in mainmenu.tscn

--- Stardust-Rescue-Run/script/mainmenu.gd
+++ Stardust-Rescue-Run/script/mainmenu.gd
@@ -2,16 +2,19 @@ extends MarginContainer
 
 @onready var start = %start
 @onready var exit = %exit
-
-
+@onready var credits = %credits
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	start.pressed.connect(_on_start_pressed)
 	exit.pressed.connect(_on_exit_pressed)
+	credits.pressed.connect(_on_credits_pressed)
 	
 func _on_start_pressed():
 	Game.next_level()
 
 func _on_exit_pressed():
 	get_tree().quit()
+	
+func _on_credits_pressed():
+	get_tree().change_scene_to_file("res://scenes/credits.tscn")
