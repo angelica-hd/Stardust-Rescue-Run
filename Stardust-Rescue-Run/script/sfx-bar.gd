f394e713d0654b1de4376ae6238c42ae90a3370b
Ignacio <fuentesovalle57@gmail.com>
2023-07-07 14:56:33 -0400

    Fix volume bars
    Ahora, se mantiene el nivel de volumen que se aplica.

--- Stardust-Rescue-Run/script/sfx-bar.gd
+++ Stardust-Rescue-Run/script/sfx-bar.gd
@@ -1,6 +1,10 @@
 extends HSlider
 
 var sfx_bus = AudioServer.get_bus_index("SFX")
+@onready var h_slider = $"."
+
+func _ready():
+	h_slider.value =AudioServer.get_bus_volume_db(sfx_bus)
 
 func _on_value_changed(value):
 	AudioServer.set_bus_volume_db(sfx_bus, value)
--- Stardust-Rescue-Run/script/volume-bar.gd
+++ Stardust-Rescue-Run/script/volume-bar.gd
@@ -1,6 +1,10 @@
 extends Control
 
 var Niveles_bus = AudioServer.get_bus_index("Niveles")
+@onready var h_slider = $HSlider
+	
+func _ready():
+	h_slider.value =AudioServer.get_bus_volume_db(Niveles_bus)
 	
 func _on_h_slider_value_changed(value):
 	AudioServer.set_bus_volume_db(Niveles_bus, value)
