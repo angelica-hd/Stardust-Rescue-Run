f433f4499652d2de0d736d3f3286c6848eca71da
Ignacio <fuentesovalle57@gmail.com>
2023-07-07 00:03:28 -0400

    Add finish menu and fix music errors

---
+++ Stardust-Rescue-Run/script/finish.gd
@@ -0,0 +1,19 @@
+extends MarginContainer
+@onready var menuu = %menuu
+@onready var restart = %restart
+
+	
+func _ready():
+	menuu.pressed.connect(_on_menuu_pressed)
+	restart.pressed.connect(_on_restart_pressed)
+
+
+func _on_menuu_pressed():
+	$Confirm.play()
+	await($Confirm.finished)
+	get_tree().change_scene_to_file("res://scenes/mainmenu.tscn")
+	
+func _on_restart_pressed():
+	$Confirm.play()
+	await($Confirm.finished)
+	get_tree().change_scene_to_file("res://scenes/nivel2.tscn")
--- Stardust-Rescue-Run/script/portal.gd
+++ Stardust-Rescue-Run/script/portal.gd
@@ -10,3 +10,4 @@ func _on_portal_body_entered(body):
 	if body.name == "SPACESHIP":
 		if body.monitos == Game.get_level_monitos():
 			get_tree().change_scene_to_file("res://scenes/win_menu.tscn")
+
