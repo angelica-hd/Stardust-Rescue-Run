2afea67526556990c9b23588343c7806975afdbe
Ignacio <fuentesovalle57@gmail.com>
2023-07-20 12:53:26 -0400

    fix volume bar

--- Stardust-Rescue-Run/script/menu_volume.gd
+++ Stardust-Rescue-Run/script/menu_volume.gd
@@ -2,7 +2,11 @@ extends HSlider
 
 
 var menu_bus = AudioServer.get_bus_index("Niveles")
+@onready var menu_volume = $"."
 
+func _ready():
+	menu_volume.value =AudioServer.get_bus_volume_db(menu_bus)
+	
 func _on_value_changed(value):
 	AudioServer.set_bus_volume_db(menu_bus, value)
 	if value == -30:
